9081b6ac6c88f71ec1a43f638b113f792e27da56
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-06 14:59:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,31 +38,14 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(0, array.length-1)
+  kittens.slice(0, 2)
 }
 
 function removeFirstKitten(){
-  kittens.slice
+  kittens.slice(1, 3)
 }
 
-describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
 
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-
-  describe('removeFirstKitten()', function() {
-    it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      window.kittens = ['Milo', 'Otis', 'Garfield'];
-      expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
-})
 
 
 
