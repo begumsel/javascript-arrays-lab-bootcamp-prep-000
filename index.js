64162488a0873a642640cfb83565f39a9826a804
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-06 14:36:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ function destructivelyAppendKitten(name){
   return window.kittens
 }
 
+function destructivelyPrependKitten(name){
+  
+}
+
 describe('destructivelyPrependKitten(name)', function() {
     it('prepends a kitten to the beginning of the kittens array', function() {
       window.kittens = ['Milo', 'Otis', 'Garfield'];
