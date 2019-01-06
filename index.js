0313765461143431017922adb976893d179bee00
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-06 14:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
-function  kittens(){
-  
+function  Arrays(){
+  function kittens(){
+    var kittens = ["Milo", "Otis", "Garfield"]
+  }
 } 
+
+describe('Arrays', function() {
+  describe('kittens', function() {
+    it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
